074eaf654cac24b83673a922255b4e8f9a082987
HarkiratGill007 <gillharkirat2007@gmail.com>
2026-09-09 22:53:46 +0530

    minor correction edit

--- Character.cpp
+++ Character.cpp
@@ -4,35 +4,35 @@
 // std::string add_energy_signed(int amount) override;
 
 std ::string MainChar::add_energy_signed(int amt){
-    if ((100 <= amt) &&  (amt > 0)){
+    if ((100 >= amt) &&  (amt > 0)){
         energy += amt;
-        return "Your energy increased";
         if (energy > 100) energy = 100;
+        return "Your energy increased";
     }
     else if ((amt >= -100) && (amt < 0)){
-        energy -= amt;
-        return "Your energy decreased";
+        energy += amt;
         if (energy < 0) energy = 0;
+        return "Your energy decreased";
     }
     else return "DevError : Invalid Amount Probably" ;
      
 }
 
 std ::string SideChar::add_energy_signed(int amt){
-    if ((100 <= amt) &&  (amt > 0)){
+    if ((100 >= amt) &&  (amt > 0)){
         energy += amt;
-        return name + "'s energy increased";
         if (energy > 100) energy = 100;
+        return name + "'s energy increased";
     }
     else if ((amt >= -100) && (amt < 0)){
-        energy -= amt;
-        return name + "'s energy decreased";
+        energy += amt;
         if (energy < 0) energy = 0;
+        return name + "'s energy decreased";
     }
     else return "DevError : Invalid Amount Probably" ;
 }
 
-std::string MainChar::change_details(std::string new_name = '\0', char new_gender = '\0'){
+std::string MainChar::change_details(std::string new_name , char new_gender  ){
     bool any_change = 0;
     if (new_name[0] != '\0'){
         name = new_name;
--- Character.h
+++ Character.h
@@ -9,12 +9,13 @@ class Mortal{
     int energy ;
     char gender ;
     Mortal(std::string _name , int _age , int _ene , char _gen): name(_name) , age(_age) ,energy(_ene), gender(_gen){};
+    virtual ~Mortal(){}
     virtual std::string add_energy_signed(int amount) = 0;//used for decraeasing too
 };
 
 class MainChar : public Mortal{
     public:
-    MainChar(std::string _name , int _age ,int _ene , char _gen , Inventory _inv): Mortal(_name, _age, _ene = 100,  _gen){mc_inventory = _inv;}
+    MainChar(std::string _name , int _age ,int _ene  , char _gen , Inventory _inv): Mortal(_name, _age, _ene ,  _gen){mc_inventory = _inv;}
     Inventory mc_inventory;
     std::string change_details(std::string new_name = '\0', char new_gender = '\0');
     std::string add_energy_signed(int amount) override;
@@ -22,7 +23,7 @@ class MainChar : public Mortal{
 
 class SideChar : public Mortal{
     public:
-    SideChar(std::string _name , int _age ,char _gen , Inventory _inv , int _ene = 15): Mortal(_name, _age, _ene = 15,  _gen){sc_inventory = _inv;}
+    SideChar(std::string _name , int _age ,char _gen , Inventory _inv , int _ene): Mortal(_name, _age, _ene,  _gen){sc_inventory = _inv;}
     Inventory sc_inventory;
     std::string add_energy_signed(int amount) override;
 
